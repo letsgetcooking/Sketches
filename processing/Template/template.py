1208b6c213e688d7a13b8f2373067fc63af605e6
letsgetcooking <letsgetcooking@users.noreply.github.com>
2015-06-18 18:05:28 +0300

    Create template.py

---
+++ processing/Template/template.py
@@ -0,0 +1,37 @@
+W = H = 500
+FPS = 20.0
+DURATION = 4
+N_FRAMES = DURATION * FPS
+N_SAMPLES = 4
+RECORD = False
+
+
+def draw_(t):
+    pass
+
+def setup():
+    size(W, H)
+    frameRate(FPS)
+
+def draw():
+    if not RECORD:
+        t = (frameCount / float(N_FRAMES)) % 1.0
+        draw_(t)
+    else:
+        result = [[0, 0, 0] for i in range(W * H)]
+        for sample in range(N_SAMPLES):
+            t = (frameCount + sample / float(N_SAMPLES)) / N_FRAMES
+            draw_(t)
+            loadPixels()
+            for i, pix in enumerate(pixels):
+                result[i][0] += red(pix)
+                result[i][1] += green(pix)
+                result[i][2] += blue(pix)
+        loadPixels()
+        for i, rgb in enumerate(result):
+            pixels[i] = color(rgb[0] / N_SAMPLES, rgb[1] / N_SAMPLES, rgb[2] / N_SAMPLES)
+        updatePixels()
+        if frameCount <= N_FRAMES:
+            saveFrame('gif/####.gif')
+        else:
+            exit()
