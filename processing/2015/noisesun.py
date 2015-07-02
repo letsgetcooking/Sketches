6d317ba3a3de2c45889398263942c494496315f2
letsgetcooking <letsgetcooking@users.noreply.github.com>
2015-07-03 00:03:39 +0300

    Update noisesun.py

--- processing/2015/noisesun.py
+++ processing/2015/noisesun.py
@@ -12,6 +12,7 @@ RECORD = False
 
 
 def polar2cart(r,theta):
+    """ polar coordinates to cartesian """
     return r * cos(theta), r * sin(theta)
 
 def draw_(t):
@@ -19,6 +20,7 @@ def draw_(t):
     translate(W / 2, H / 2)
     background(BG_COLOR)
 
+    # drawing mask and filling it with noise
     stroke(MASK_COLOR)
     fill(MASK_COLOR)
     ellipse(0, 0, 2 * RAD, 2 * RAD)
@@ -30,11 +32,13 @@ def draw_(t):
                 pixels[i * W + j] = lerpColor(DARK_COLOR, MAIN_COLOR, noise_v)
     updatePixels()
 
+    # drawing sun contour
     stroke(MAIN_COLOR)
     strokeWeight(5)
     noFill()
     ellipse(0, 0, 2 * RAD, 2 * RAD)
 
+    # drowing sun beams
     n = 160.0
     for i in range(n):
         stroke(MAIN_COLOR)
@@ -45,10 +49,12 @@ def draw_(t):
         x2, y2 = polar2cart(150 + noise_v * 100, i / n * 2 * PI)
         line(x1, y1, x2, y2)
 
+    # drawing noise value inside circle
     x_noise, y_noise = polar2cart(RAD, (t * n) / n * 2 * PI)
     noise_v = noise(x_noise * 0.02 + 100, y_noise * 0.02 + 200)
     fill(lerpColor(DARK_COLOR, MAIN_COLOR, noise_v))
     ellipse(x_noise, y_noise, 20, 20)
+
     popMatrix()
 
 def setup():
@@ -56,13 +62,14 @@ def setup():
     frameRate(FPS)
 
 def draw():
+    # drawing with SSAA when recording
     if not RECORD:
         t = (frameCount / float(N_FRAMES)) % 1.0
         draw_(t)
     else:
         result = [[0, 0, 0] for i in range(W * H)]
         for sample in range(N_SAMPLES):
-            t = (frameCount + 0.2 * sample / float(N_SAMPLES)) / N_FRAMES
+            t = (frameCount + sample / float(N_SAMPLES)) / N_FRAMES
             draw_(t)
             loadPixels()
             for i, pix in enumerate(pixels):
