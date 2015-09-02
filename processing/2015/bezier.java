013091b122bc18ed2440c96123fda5428c1895d8
letsgetcooking <letsgetcooking@users.noreply.github.com>
2015-09-02 07:28:13 +0300

    Update bezier.java

--- processing/2015/bezier.java
+++ processing/2015/bezier.java
@@ -1,9 +1,10 @@
 color active_color = color(255, 0, 0);
-color passive_color = color(255);
-color passive_color_static = color(35);
+color passive_color = color(80);
 color bg_color = color(0);
 
-int rad = 10;
+int w = 500;
+int h = 500;
+int rad = 30;
 int score;
 int drawing_mode;
 
@@ -41,22 +42,23 @@ class MCurve
   }
 }
 
+Point active_point;
 ArrayList<MCurve> mcurves;
 
 boolean check()
 {
   PGraphics pg = createGraphics(width, height);
   pg.beginDraw();
-  pg.background(bg_color);
+  pg.background(0);
   pg.noStroke();
   pg.fill(100, 60);
   
   for (MCurve mcurve : mcurves)
   {
-    pg.bezier(mcurve.a.xpos, mcurve.a.ypos,
-              mcurve.b.xpos, mcurve.b.ypos,
-              mcurve.c.xpos, mcurve.c.ypos,
-              mcurve.d.xpos, mcurve.d.ypos);
+    pg.bezier(mcurve.a.xpos + width / 2, mcurve.a.ypos + height / 2,
+           mcurve.b.xpos + width / 2, mcurve.b.ypos + height / 2,
+           mcurve.c.xpos + width / 2, mcurve.c.ypos + height / 2,
+           mcurve.d.xpos + width / 2, mcurve.d.ypos + height / 2);
   }
   
   pg.endDraw();
@@ -72,40 +74,48 @@ boolean check()
   
   if (max_brightness > 30.0)
     return false;
-  else
-    return true;
+  return true;
 }
 
 void draw_playing()
 {
   background(bg_color);
-  stroke(255);
+  noStroke();
   
   for (MCurve mcurve : mcurves)
   {
-    noFill();
-    bezier(mcurve.a.xpos, mcurve.a.ypos,
-           mcurve.b.xpos, mcurve.b.ypos,
-           mcurve.c.xpos, mcurve.c.ypos,
-           mcurve.d.xpos, mcurve.d.ypos);
-           
     for (Point p : mcurve.points)
     {
       if (p.is_active)
+      {
         fill(active_color);
-      else if (p.is_static)
-        fill(passive_color_static);
-      else
+        ellipse(p.xpos + width / 2, p.ypos + height / 2, rad, rad);
+      }
+      else if (!p.is_static)
+      {
         fill(passive_color);
-      ellipse(p.xpos, p.ypos, rad, rad);
+        ellipse(p.xpos + width / 2, p.ypos + height / 2, rad, rad);
+      }
     }
+  }
+
+  stroke(255);
+  for (MCurve mcurve : mcurves)
+  {
+    noFill();
+    bezier(mcurve.a.xpos + width / 2, mcurve.a.ypos + height / 2,
+           mcurve.b.xpos + width / 2, mcurve.b.ypos + height / 2,
+           mcurve.c.xpos + width / 2, mcurve.c.ypos + height / 2,
+           mcurve.d.xpos + width / 2, mcurve.d.ypos + height / 2);
   }
   
   fill(200);
-  textSize(18);
-  textAlign(LEFT);
-  text("SCORE: " + score, 10, height - 35);
-  text("PRESS C TO CHECK", 10, height - 15);
+  textAlign(CENTER);
+  textSize(30);
+  text("UNTANGLE IT", width / 2, 40);
+  textSize(20);
+  text("TIME: " + score, width / 2, height - 35);
+  text("PRESS C TO CHECK", width / 2, height - 15);
   
   if ((frameCount % 6) == 0)
     score += 1;
@@ -119,89 +129,37 @@ void draw_finished()
   for (MCurve mcurve : mcurves)
   {
     noFill();
-    bezier(mcurve.a.xpos, mcurve.a.ypos,
-           mcurve.b.xpos, mcurve.b.ypos,
-           mcurve.c.xpos, mcurve.c.ypos,
-           mcurve.d.xpos, mcurve.d.ypos);
+    bezier(mcurve.a.xpos + width / 2, mcurve.a.ypos + height / 2,
+           mcurve.b.xpos + width / 2, mcurve.b.ypos + height / 2,
+           mcurve.c.xpos + width / 2, mcurve.c.ypos + height / 2,
+           mcurve.d.xpos + width / 2, mcurve.d.ypos + height / 2);
   }
   
   fill(active_color);
   textAlign(CENTER);
-  textSize(72);
-  text(score, width / 2, height / 2);
+  textSize(30);
+  text("CONGRATULATIONS!", width / 2, 40);
   textSize(20);
-  text("PRESS N TO START A NEW GAME", width / 2, height / 2 + 25);
+  text("YOUR BEST TIME: " + score, width / 2, height - 35);
+  text("PRESS N TO START A NEW GAME", width / 2, height - 15);
 }
 
-void keyPressed()
+void reset_all()
 {
-  if ((key == 'c') && (check()))
-    drawing_mode = (drawing_mode + 1) % 2;
-  else if ((key == 'n') && (drawing_mode == 1))
-    setup();
-}
-
-void mouseDragged()
-{
-  for (MCurve mcurve : mcurves)
-  {
-    for (Point p : mcurve.points)
-    {
-      if (p.is_active)
-      {
-        p.xpos = constrain(mouseX, rad / 2, width - rad / 2);
-        p.ypos = constrain(mouseY, rad / 2, height - rad / 2);
-        return;
-      }
-    }
-  }
-}
-
-void mousePressed()
-{
-  for (MCurve mcurve : mcurves)
-  {
-    for (Point p : mcurve.points)
-    {
-      if ((sqrt(pow(p.xpos - mouseX, 2) + pow(p.ypos - mouseY, 2)) < rad)
-        && !p.is_static)
-      {
-        p.is_active = true;
-        return;
-      }
-    }
-  }
-}
-
-void mouseReleased()
-{
-  for (MCurve mcurve : mcurves)
-  {
-    mcurve.a.is_active = false;
-    mcurve.b.is_active = false;
-    mcurve.c.is_active = false;
-    mcurve.d.is_active = false;
-  }
-}
-
-void setup()
-{
-  size(640, 360);
-  
   float n = 12.0;
   int r = 100;
   mcurves = new ArrayList<MCurve>();
   
-  float prev_point_x = r * cos(0) + width / 2;
-  float prev_point_y = r * sin(0) + height / 2;
+  float prev_point_x = r * cos(0);
+  float prev_point_y = r * sin(0);
   for (int i = 0; i < n; i++)
   {
-    float next_point_x = r * cos((i + 1) / n * TWO_PI) + width / 2;
-    float next_point_y = r * sin((i + 1) / n * TWO_PI) + height / 2;
+    float next_point_x = r * cos((i + 1) / n * TWO_PI);
+    float next_point_y = r * sin((i + 1) / n * TWO_PI);
     
     Point a = new Point(prev_point_x, prev_point_y, false, true);
-    Point b = new Point(random(width - rad) + rad / 2, random(height - rad) + rad / 2, false, false);
-    Point c = new Point(random(width - rad) + rad / 2, random(height - rad) + rad / 2, false, false);
+    Point b = new Point(random(w - rad) + rad / 2 - w / 2, random(h - rad) + rad / 2 - h / 2, false, false);
+    Point c = new Point(random(w - rad) + rad / 2 - w / 2, random(h - rad) + rad / 2 - h / 2, false, false);
     Point d = new Point(next_point_x, next_point_y, false, true);
 
     MCurve mcurve = new MCurve(a, b, c, d);
@@ -215,14 +173,65 @@ void setup()
   drawing_mode = 0;
 }
 
+void keyPressed()
+{
+  if ((key == 'c') && (drawing_mode == 0) && (check()))
+    drawing_mode = (drawing_mode + 1) % 2;
+  else if ((key == 'n') && (drawing_mode == 1))
+    reset_all();
+}
+
+void mouseDragged()
+{
+  if (active_point != null)
+  {
+    active_point.xpos = constrain(mouseX, rad / 2, width - rad / 2) - width / 2;
+    active_point.ypos = constrain(mouseY, rad / 2, height - rad / 2) - height / 2;
+  }
+}
+
+void mousePressed()
+{
+  Point closest = null;
+  float min_dist = 99999999.9;
+  for (MCurve mcurve : mcurves)
+  {
+    for (Point p : mcurve.points)
+    {
+      float dist = sqrt(pow(p.xpos - mouseX + width / 2, 2) + pow(p.ypos - mouseY + height / 2, 2));
+      if ((dist < rad) && !p.is_static && dist < min_dist)
+      {
+        min_dist = dist;
+        closest = p;
+      }
+    }
+  }
+  if (closest != null)
+  {
+    closest.is_active = true;
+    active_point = closest;
+  }
+}
+
+void mouseReleased()
+{
+  if (active_point != null)
+  {
+    active_point.is_active = false;
+    active_point = null;
+  }
+}
+
+void setup()
+{
+  size(640, 360);
+  reset_all();
+}
+
 void draw()
 {
   if (drawing_mode == 0)
-  {
     draw_playing();
-  }
   else
-  {
     draw_finished();
-  }
 }
